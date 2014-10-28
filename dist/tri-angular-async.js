4394b16865ba03e79c08d93e5c6adde3e0150df6
Jakub Strojewski <jakub.strojewski@gmail.com>
2014-10-28 17:29:31 +0100

    Custom or no scope to digest (fix)

--- dist/tri-angular-async.js
+++ dist/tri-angular-async.js
@@ -15,31 +15,50 @@
                 return window.setTimeout(fnCaller, 21);
             };
 
-        var asyncService = function ($log, $root) {
+        var asyncService = function ($log, $rootScope) {
+
+            // mode can be a $scope, any truth-y value (will prevent $digest()) or may not be
+            // so old API works just like it has
             var future = function (fn, mode) {
-                var scope = $root;
+
+                // normally lets $rootScope.$digest()
+                var scope = $rootScope;
 
                 if (mode) {
-                    scope = angular.isFunction(mode.$destroy) ? mode : null;
+                    // but if scope is passed, let's call scope.$digest() or
+                    // if other truth-y arg is passed (not a scope) just
+                    // don't $digest()
+                    scope = angular.isFunction(mode.$digest) ? mode : null;
                 }
 
+                // as we know proper mode (clean/localScope/rootScope) we can
+                // run the callback in some near future
                 return async(function () {
+
+                    // user may break something
                     try {
                         fn();
                     } catch (e) {
+                        // if he does - let him know that it has happened in $async
                         e.message = e.message + ' :: triNgAsync.$async';
                         $log.error(e);
                     } finally {
+                        // finally let's $digest() localScope or rootScope (or do nothing)
                         scope && scope.$digest();
                     }
                 });
             };
 
             return angular.extend(future, {
+
+                // nice way to call without $digest in coffee
+                // $async.clean -> doSomething()
                 clean: function (fn) {
                     return this(fn, true);
                 },
 
+                // nice way to call with local $digest in coffee
+                // $async.clean $scope, -> doSomething()
                 local: function (scope, fn) {
                     return this(fn, scope);
                 }
